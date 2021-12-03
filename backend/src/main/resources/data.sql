e9450dab121580eda73db0efe15af8eff05a1114
MARCELO073 <marcelonogueira072@gmail.com>
2021-12-03 17:49:35 -0300

    Lesson seed, fix:lessondone

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -28,6 +28,20 @@ INSERT INTO tb_section (titles,description, position,img_Uri, resource_id, prere
 INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refound_Moment, available, only_Update) VALUES (1, 1, TIMESTAMP WITH TIME ZONE '2020-12-20T13:00:00Z', null,true, false);
 INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refound_Moment, available, only_Update) VALUES (2, 1, TIMESTAMP WITH TIME ZONE '2020-12-20T13:00:00Z', null,true, false);
 
+INSERT INTO tb_lesson ( title, position, section_id ) VALUES ('Aula 1 do capitulo 1',1 ,1);
+INSERT INTO tb_content (id, text_Content,video_Uri) VALUES (1, 'Material e apoio: ABC', 'https://youtu.be/Xm8ttgu02Zw');
+
+INSERT INTO tb_lesson ( title, position, section_id ) VALUES ('Aula 2 do capitulo 1',2 ,1);
+INSERT INTO tb_content (id, text_Content,video_Uri) VALUES (2, '', 'https://youtu.be/Xm8ttgu02Zw');
+
+INSERT INTO tb_lesson ( title, position, section_id ) VALUES ('Aula 3 do capitulo 1',3 ,1);
+INSERT INTO tb_content (id, text_Content,video_Uri) VALUES (3, '', 'https://youtu.be/Xm8ttgu02Zw');
+
+INSERT INTO tb_lesson ( title, position, section_id ) VALUES ('Tarefa do capitulo 1',4 ,1);
+INSERT INTO tb_task ( id, description, question_Count, approval_Count, weight, due_Date) VALUES ( 4, 'fazer um trabalho legal', 5, 4, 1.0, TIMESTAMP WITH TIME ZONE '2020-12-25T13:00:00Z');
+
+INSERT INTO tb_lesson_done(lesson_id, user_id, offer_id) VALUES (1, 1, 1);
+INSERT INTO tb_lesson_done(lesson_id, user_id, offer_id) VALUES (2, 1, 1);
 
 
 
